44eeb4a438a0c5c6efec59f1862b6f80043e765c
Tom Power <tpower21@gmail.com>
2025-08-02 06:50:41 +0100

    drop sixtyPercentShura

--- core/src/main/kotlin/se/tp21/sixtypercent/karabiner/sixtyPercentModificationMap.kt
+++ core/src/main/kotlin/se/tp21/sixtypercent/karabiner/sixtyPercentModificationMap.kt
@@ -2,7 +2,6 @@ package se.tp21.sixtypercent.karabiner
 
 import se.tp21.sixtypercent.karabiner.modifications.sixtyPercent
 import se.tp21.sixtypercent.karabiner.modifications.sixtyPercentK12
-import se.tp21.sixtypercent.karabiner.modifications.sixtyPercentShura
 import se.tp21.sixtypercent.karabiner.modifications.sixtyPercentYoda
 import sh.kau.karabiner.ComplexModifications
 
@@ -10,6 +9,5 @@ val sixtyPercentModificationsMap: Map<String, ComplexModifications> =
     mapOf(
         "sixtyPercent" to sixtyPercent(),
         "sixtyPercent_K12" to sixtyPercentK12(),
-        "sixtyPercent_Shura" to sixtyPercentShura(),
         "sixtyPercent_Yoda" to sixtyPercentYoda(),
     )
