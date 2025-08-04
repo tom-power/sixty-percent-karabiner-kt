72eb0e480d69a1c11fc691fde0e08e33ca9a1417
Tom Power <tp21@tuta.io>
2025-08-04 12:51:30 +0100

    update to use karabinerRule {}

--- core/src/main/kotlin/se/tp21/sixtypercent/karabiner/modifications/sixtyPercentYoda.kt
+++ core/src/main/kotlin/se/tp21/sixtypercent/karabiner/modifications/sixtyPercentYoda.kt
@@ -4,9 +4,13 @@ import sh.kau.karabiner.ComplexModifications
 import sh.kau.karabiner.From
 import sh.kau.karabiner.FromModifiers
 import sh.kau.karabiner.KeyCode
+import sh.kau.karabiner.KeyCode.Backslash
+import sh.kau.karabiner.KeyCode.Escape
+import sh.kau.karabiner.KeyCode.Slash
 import sh.kau.karabiner.Manipulator
 import sh.kau.karabiner.ModifierKeyCode
 import sh.kau.karabiner.ModifierKeyCode.LeftControl
+import sh.kau.karabiner.ModifierKeyCode.LeftOption
 import sh.kau.karabiner.ModifierKeyCode.LeftShift
 import sh.kau.karabiner.ModifierKeyCode.RightShift
 import sh.kau.karabiner.To
@@ -15,34 +19,24 @@ import sh.kau.karabiner.karabinerRule
 fun sixtyPercentYoda() = ComplexModifications(
     title = "sixtyPercentYoda",
     rules = listOf(
-        karabinerRule(
-            description = "~/ (right_shift + escape)",
-            manipulators = listOf(
-                Manipulator(
-                    from = From.with(
-                        KeyCode.Escape,
-                        FromModifiers(mandatory = listOf(ModifierKeyCode.RightShift))
-                    ),
-                    to = listOf(
-                        To(KeyCode.Backslash, modifiers = listOf(ModifierKeyCode.LeftShift)),
-                        To(KeyCode.Slash)
-                    )
+        karabinerRule {
+            description = "~/ (right_shift + escape)"
+            mapping {
+                from = From(Escape, FromModifiers(mandatory = listOf(RightShift)))
+                to = listOf(
+                    To(Backslash, listOf(LeftShift)),
+                    To(Slash)
                 )
-            ).toTypedArray(),
-        ),
-        karabinerRule(
-            description = "~ (left_option + escape)",
-            manipulators = listOf(
-                Manipulator(
-                    from = From.with(
-                        KeyCode.Escape,
-                        FromModifiers(mandatory = listOf(ModifierKeyCode.LeftOption))
-                    ),
-                    to = listOf(
-                        To(KeyCode.Backslash, modifiers = listOf(ModifierKeyCode.LeftShift))
-                    )
+            }
+        },
+        karabinerRule {
+            description = "~ (left_option + escape)"
+            mapping {
+                from = From(Escape, FromModifiers(mandatory = listOf(LeftOption)))
+                to = listOf(
+                    To(Backslash, listOf(LeftShift)),
                 )
-            ).toTypedArray(),
-        )
+            }
+        }
     )
 )
