e74fb9152b40dd86811446664ddafecf9048c1f9
Tom Power <tpower21@gmail.com>
2025-08-12 11:18:44 +0100

    add mac keys with command

--- core/src/main/kotlin/se/tp21/sixtypercent/karabiner/modifications/sixtyPercentMacKeys.kt
+++ core/src/main/kotlin/se/tp21/sixtypercent/karabiner/modifications/sixtyPercentMacKeys.kt
@@ -9,72 +9,142 @@ fun sixtyPercentMacKeys() =
         rules = rules()
     )
 
-private fun rules(): List<KarabinerRule> =
-    listOf(
-        karabinerRule {
-            description = "brightness decrement (left_control + f1)"
-            mapping {
-                fromKey = KeyCode.F1
-                fromModifiers = FromModifiers(
-                    mandatory =
-                        listOf(ModifierKeyCode.LeftControl)
-                )
-                toKey = DisplayBrightnessDecrement
-            }
-        },
-        karabinerRule {
-            description = "brightness increment (left_control + f2)"
-            mapping {
-                fromKey = KeyCode.F2
-                fromModifiers = FromModifiers(
-                    mandatory =
-                        listOf(ModifierKeyCode.LeftControl)
-                )
-                toKey = DisplayBrightnessIncrement
-            }
-        },
-        karabinerRule {
-            description = "mission control (left_control + f3)"
-            mapping {
-                fromKey = KeyCode.F3
-                fromModifiers = FromModifiers(
-                    mandatory =
-                        listOf(ModifierKeyCode.LeftControl)
-                )
-                toKey = MissionControl
-            }
-        },
-        karabinerRule {
-            description = "mute (left_control + f10)"
-            mapping {
-                fromKey = KeyCode.F10
-                fromModifiers = FromModifiers(
-                    mandatory =
-                        listOf(ModifierKeyCode.LeftControl)
-                )
-                toKey = Mute
-            }
-        },
-        karabinerRule {
-            description = "volume decrement (left_control + f11)"
-            mapping {
-                fromKey = KeyCode.F11
-                fromModifiers = FromModifiers(
-                    mandatory =
-                        listOf(ModifierKeyCode.LeftControl)
-                )
-                toKey = VolumeDecrement
-            }
-        },
-        karabinerRule {
-            description = "volume increment (left_control + f12)"
-            mapping {
-                fromKey = KeyCode.F12
-                fromModifiers = FromModifiers(
-                    mandatory =
-                        listOf(ModifierKeyCode.LeftControl)
-                )
-                toKey = VolumeIncrement
-            }
+private fun rules(): List<KarabinerRule> = macKeysFnCommand() + macKeysFn()
+
+private fun macKeysFnCommand(): List<KarabinerRule> = listOf(
+    karabinerRule {
+        description = "brightness decrement (left_control + right_command + 1)"
+        mapping {
+            fromKey = Num1
+            fromModifiers = FromModifiers(
+                mandatory =
+                    listOf(ModifierKeyCode.LeftControl, ModifierKeyCode.RightCommand)
+            )
+            toKey = DisplayBrightnessDecrement
         }
-    )
+    },
+    karabinerRule {
+        description = "brightness increment (left_control + right_command + 2)"
+        mapping {
+            fromKey = Num2
+            fromModifiers = FromModifiers(
+                mandatory =
+                    listOf(ModifierKeyCode.LeftControl, ModifierKeyCode.RightCommand)
+            )
+            toKey = DisplayBrightnessIncrement
+        }
+    },
+    karabinerRule {
+        description = "mission control (left_control + right_command + 3)"
+        mapping {
+            fromKey = Num3
+            fromModifiers = FromModifiers(
+                mandatory =
+                    listOf(ModifierKeyCode.LeftControl, ModifierKeyCode.RightCommand)
+            )
+            toKey = MissionControl
+        }
+    },
+    karabinerRule {
+        description = "mute (left_control + right_command + 0)"
+        mapping {
+            fromKey = Num0
+            fromModifiers = FromModifiers(
+                mandatory =
+                    listOf(ModifierKeyCode.LeftControl, ModifierKeyCode.RightCommand)
+            )
+            toKey = Mute
+        }
+    },
+    karabinerRule {
+        description = "volume decrement (left_control + right_command + hyphen)"
+        mapping {
+            fromKey = Hyphen
+            fromModifiers = FromModifiers(
+                mandatory =
+                    listOf(ModifierKeyCode.LeftControl, ModifierKeyCode.RightCommand)
+            )
+            toKey = VolumeDecrement
+        }
+    },
+    karabinerRule {
+        description = "volume increment (left_control + equals)"
+        mapping {
+            fromKey = EqualSign
+            fromModifiers = FromModifiers(
+                mandatory =
+                    listOf(ModifierKeyCode.LeftControl, ModifierKeyCode.RightCommand)
+            )
+            toKey = VolumeIncrement
+        }
+    }
+)
+
+private fun macKeysFn(): List<KarabinerRule> = listOf(
+    karabinerRule {
+        description = "brightness decrement (left_control + f1)"
+        mapping {
+            fromKey = F1
+            fromModifiers = FromModifiers(
+                mandatory =
+                    listOf(ModifierKeyCode.LeftControl)
+            )
+            toKey = DisplayBrightnessDecrement
+        }
+    },
+    karabinerRule {
+        description = "brightness increment (left_control + f2)"
+        mapping {
+            fromKey = F2
+            fromModifiers = FromModifiers(
+                mandatory =
+                    listOf(ModifierKeyCode.LeftControl)
+            )
+            toKey = DisplayBrightnessIncrement
+        }
+    },
+    karabinerRule {
+        description = "mission control (left_control + f3)"
+        mapping {
+            fromKey = F3
+            fromModifiers = FromModifiers(
+                mandatory =
+                    listOf(ModifierKeyCode.LeftControl)
+            )
+            toKey = MissionControl
+        }
+    },
+    karabinerRule {
+        description = "mute (left_control + f10)"
+        mapping {
+            fromKey = F10
+            fromModifiers = FromModifiers(
+                mandatory =
+                    listOf(ModifierKeyCode.LeftControl)
+            )
+            toKey = Mute
+        }
+    },
+    karabinerRule {
+        description = "volume decrement (left_control + f11)"
+        mapping {
+            fromKey = F11
+            fromModifiers = FromModifiers(
+                mandatory =
+                    listOf(ModifierKeyCode.LeftControl)
+            )
+            toKey = VolumeDecrement
+        }
+    },
+    karabinerRule {
+        description = "volume increment (left_control + f12)"
+        mapping {
+            fromKey = F12
+            fromModifiers = FromModifiers(
+                mandatory =
+                    listOf(ModifierKeyCode.LeftControl)
+            )
+            toKey = VolumeIncrement
+        }
+    }
+)
