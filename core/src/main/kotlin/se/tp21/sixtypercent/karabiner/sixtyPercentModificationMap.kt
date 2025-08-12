8250a8eebf2d6da89fe8bea0b531175bcacd66b4
Tom Power <tp21@tuta.io>
2025-08-12 07:41:19 +0100

    add mac keys

--- core/src/main/kotlin/se/tp21/sixtypercent/karabiner/sixtyPercentModificationMap.kt
+++ core/src/main/kotlin/se/tp21/sixtypercent/karabiner/sixtyPercentModificationMap.kt
@@ -1,12 +1,14 @@
 package se.tp21.sixtypercent.karabiner
 
 import se.tp21.sixtypercent.karabiner.modifications.sixtyPercentFunctionKeys
+import se.tp21.sixtypercent.karabiner.modifications.sixtyPercentMacKeys
 import se.tp21.sixtypercent.karabiner.modifications.sixtyPercentNavigation
 import se.tp21.sixtypercent.karabiner.modifications.sixtyPercentSymbols
 import sh.kau.karabiner.ComplexModifications
 
 val sixtyPercentModificationsMap: Map<String, ComplexModifications> =
     mapOf(
+        "sixtyPercentMacKeys" to sixtyPercentMacKeys(),
         "sixtyPercentFunctionKeys" to sixtyPercentFunctionKeys(),
         "sixtyPercentNavigation" to sixtyPercentNavigation(),
         "sixtyPercentSymbols" to sixtyPercentSymbols(),
