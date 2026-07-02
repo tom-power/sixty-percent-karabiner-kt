ef436d8931bee720751b72d9e97647d5b9230de3
Tom Power <tp21@tuta.io>
2026-07-02 14:40:37 +0100

    drop mac keys

--- core/src/main/kotlin/se/tp21/sixtypercent/karabiner/sixtyPercentModificationMap.kt
+++ core/src/main/kotlin/se/tp21/sixtypercent/karabiner/sixtyPercentModificationMap.kt
@@ -1,7 +1,6 @@
 package se.tp21.sixtypercent.karabiner
 
 import se.tp21.sixtypercent.karabiner.modifications.sixtyPercentFunctionKeys
-import se.tp21.sixtypercent.karabiner.modifications.sixtyPercentMacKeys
 import se.tp21.sixtypercent.karabiner.modifications.sixtyPercentNavigation
 import se.tp21.sixtypercent.karabiner.modifications.sixtyPercentSymbols
 import sh.kau.karabiner.ComplexModifications
@@ -9,7 +8,6 @@ import sh.kau.karabiner.ComplexModifications
 val sixtyPercentModificationsMap: Map<String, ComplexModifications> =
     mapOf(
         "sixtyPercentFunctionKeys" to sixtyPercentFunctionKeys(),
-        "sixtyPercentMacKeys" to sixtyPercentMacKeys(),
         "sixtyPercentNavigation" to sixtyPercentNavigation(),
         "sixtyPercentSymbols" to sixtyPercentSymbols(),
     )
